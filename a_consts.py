81ef0fc10010550193bb41d574436adaefd98d83
CodesOfAnAlien <princemolly405@gmail.com>
2025-05-06 03:41:34 +0000

    export has been add to export the sales data in a pdf format, view expense button add to view and manage expenses, view sales button to view all sales by barbers grouped neatly under each barber and arranged by date, admin button add to acess additional features like 'add barbers', 'remove barber' add and remove service, remove sales incase user enter a wrong sales, reset data to reset the application data to make app ready for recording new set of ssales after previous sales is closed Or exported

--- a_consts.py
+++ a_consts.py
@@ -9,6 +9,24 @@ class CONSTS:
     BARBER_TXT     = "barbers.txt"
     ALL_JSON       = "all_barbers.json"
     BARBER_JSON_TMPL = "{}.json"
+    BARBER_JSON     = "barbers.json"
+    EXPENSES_JSON = "expenses.json"
+    HELP_TEXT = """
+        Welcome to BarberApp Help!
 
+        • To Record Sales: Select a barber, choose a service, enter the amount,
+        then click “Record Sale.” 
 
+        • To View History: Click “History.”  
 
+        • To Reset the App: Click “Reset” and enter admin credentials.
+
+        • To Export PDF: Click “Export” and choose a filename. 
+
+        • To View Sales: Click “View Sales.” 
+
+        • To Manage Expenses: Click “View Expense.”
+        
+
+        For more assistance, contact Developer at princemolly405@gmail.com.
+        """
